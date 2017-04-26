807be310cc5ad8d5b6d3a56783b1aad53858f059
Federico Cetraro <facetraro@gmail.com>
2017-04-26 15:41:45 -0300

    Se creo clase User

--- BlackBoards/BlackBoards/User.cs
+++ BlackBoards/BlackBoards/User.cs
@@ -1,6 +1,88 @@
-﻿namespace BlackBoards
+﻿using System;
+namespace BlackBoards
 {
     public class User
     {
+        private string name;
+        private string lastName;
+        private string email;
+        private DateTime birthDate;
+        private string password;
+
+
+        [Obsolete("constructor only usable by EntityFramework", true)]
+        public User()
+        {
+
+        }
+        public User(string name, string lastName, string email, DateTime birthDate, string password)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+            this.Email = email;
+            this.BirthDate = birthDate;
+            this.Password = password;  
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+       
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                this.email = value;
+            }
+        }
+        public DateTime BirthDate
+        {
+            get
+            {
+                return this.BirthDate;
+            }
+            set
+            {
+                this.BirthDate = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                this.password = value;
+            }
+        }
+
+
+
     }
 }
+               
