ed891b8f3dbf327dc7050338bee9d4da50bab150
Federico Cetraro <facetraro@gmail.com>
2017-04-26 16:47:18 -0300

    Refactor de user

--- BlackBoards/BlackBoardsTest/UserTest.cs
+++ BlackBoards/BlackBoardsTest/UserTest.cs
@@ -15,8 +15,17 @@ namespace BlackBoardsTest
     {
         public UserTest()
         {
-            User u = new User();
-            Assert.IsTrue(true);
+            string name = "Robert";
+            string lastN = "Belferino";
+            DateTime birth = new DateTime(1995, 04, 11);
+            string email = "RBelferino";
+            string password = "12345678";
+            User anUser = new User(name, lastN, email, birth, password);
+            Assert.Equals(name, anUser.Name);
+            Assert.Equals(lastN, anUser.LastName);
+            Assert.Equals(birth, anUser.BirthDate);
+            Assert.Equals(email, anUser.Email);
+            Assert.Equals(password, anUser.Password);
             //
             // TODO: Add constructor logic here
             //
