ed5db28a5ff340659d1b57b2249b9bb7d38e5c23
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-10 16:07:17 -0300

    Pruebas green Team

---
+++ BlackBoards/BlackBoards/Team.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoards
+{
+   public class Team
+    {
+        private String name;
+        private String description;
+        private DateTime creationDate;
+        private int maxUsers;
+        public Team() {
+
+        }
+        public Team(String aName,DateTime aCreationDate,String aDescription,int maximumUsers) {
+            this.name = aName;
+            this.creationDate = aCreationDate;
+            this.description = aDescription;
+            this.maxUsers = maximumUsers;
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                this.description = value;
+            }
+        }
+        public DateTime CreationDate
+        {
+            get
+            {
+                return this.creationDate;
+            }
+            set
+            {
+                this.creationDate = value;
+            }
+        }
+        public int MaxUsers
+        {
+            get
+            {
+                return this.maxUsers;
+            }
+            set
+            {
+                this.maxUsers = value;
+            }
+        }
+    }
+}
