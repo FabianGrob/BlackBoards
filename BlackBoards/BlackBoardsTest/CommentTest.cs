6b0065a1ed1445a31bb65dfdbf240749e236cbaa
Federico Cetraro <facetraro@gmail.com>
2017-05-10 17:04:37 -0300

    Pruebas red Comment

---
+++ BlackBoards/BlackBoardsTest/CommentTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackBoards;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackBoardsTest
+{
+    [TestClass]
+    public class CommentTest
+    {
+        [TestMethod]
+        public void TestBuilderComment()
+        {
+            User commentingUser = new Admin();
+            User resolvingUser = new Admin();
+            DateTime commentingDate = new DateTime();
+            DateTime resolvingDate = new DateTime();
+            
+            Comment aComment = new Comment(commentingUser, resolvingUser, commentingDate, resolvingDate);
+
+            Comment otherComment = new Comment();
+            otherComment.CommentingUser = commentingUser;
+            otherComment.ResolvingUser = resolvingUser;
+            otherComment.CommentingDate = commentingDate;
+            otherComment.ResolvingDate = resolvingDate;
+      
+            Assert.IsTrue(aComment.Equals(otherComment));
+        }
+    }
+}
