54b10f4836bf6cede3cb7c8b7977e10c96ba2403
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-10 15:50:11 -0300

    Pruebas red Team

---
+++ BlackBoards/BlackBoardsTest/TeamTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace BlackBoardsTest
+{
+    [TestClass]
+    public class TeamTest
+    {
+        [TestMethod]
+        public void teamTest(){
+            String name = "testName";
+            DateTime creationDate = new DateTime();
+            String description = "testDescription";
+            int maximumUsers = 10;
+
+            Team aTeam = new Team(name,creationDate,description,maximumUsers);
+            Team otherTeam = new Team();
+            otherTeam.Name = name;
+            otherTeam.CreationDate = creationDate;
+            otherTeam.Description = description;
+            otherTeam.MaxUsers = maximumUsers;
+            Assert.Equals(aTeam,otherTeam);
+
+
+            
+            }
+    }
+}
