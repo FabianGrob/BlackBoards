be60933ea11e3b5e56ea31ef1822679af287a6f9
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-11 13:33:33 -0300

    Pruebas TextBox green

---
+++ BlackBoards/BlackBoards/TextBox.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoards
+{
+    public class TextBox
+    {
+            private int width;
+            private int heigth;
+            private List<Comment>comments;
+            private int[] origin;
+            private string content;
+            private string font;
+            private int fontSize;
+
+        public TextBox() {
+            this.width = 1;
+            this.heigth = 1;
+            this.comments = new List<Comment>();
+            this.origin = new int[2];
+            this.content = "aContent";
+            this.font = "aFont";
+            this.fontSize = 10;
+    }
+        public TextBox(int aWidth, int aHeigth, List<Comment> someComments, int[] anOrigin, String aContent, string aFont, int aFontSize) {
+
+            this.width = aWidth;
+            this.heigth = aHeigth;
+            this.comments = someComments;
+            this.origin = anOrigin;
+            this.content = aContent;
+            this.font = aFont;
+            this.fontSize = aFontSize;
+
+        }
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+        public int Heigth
+        {
+            get
+            {
+                return this.heigth;
+            }
+            set
+            {
+                this.heigth = value;
+            }
+        }
+        public List<Comment> Comments
+        {
+            get
+            {
+                return this.comments;
+            }
+            set
+            {
+                this.comments = value;
+            }
+        }
+        public int[] Origin
+        {
+            get
+            {
+                return this.origin;
+            }
+            set
+            {
+                this.origin = value;
+            }
+        }
+        public string Content
+        {
+            get
+            {
+                return this.content;
+            }
+            set
+            {
+                this.content = value;
+            }
+        }
+        public string Font
+        {
+            get
+            {
+                return this.font;
+            }
+            set
+            {
+                this.font = value;
+            }
+        }
+        public int FontSize
+        {
+            get
+            {
+                return this.fontSize;
+            }
+            set
+            {
+                this.fontSize = value;
+            }
+        }
+        public override bool Equals(object aTextBox)
+        {
+            
+            return true;
+        }
+    }
+    
+}
--- BlackBoards/BlackBoardsTest/TextBoxTest.cs
+++ BlackBoards/BlackBoardsTest/TextBoxTest.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BlackBoards;
 using System.Threading.Tasks;
 
 namespace BlackBoardsTest
 {
-    class TextBoxTest
+    [TestClass]
+   public class TextBoxTest
     {
+        [TestMethod]
+        public void TestTextBoxBuilder() {
+            TextBox aTextBox = new TextBox();
+            aTextBox.Width = 1;
+            aTextBox.Heigth = 2;
+            List<Comment> comments = new List<Comment>();
+            aTextBox.Comments=comments ;
+            int[] origin = new int[2];
+            origin[0] = 1;
+            origin[1] = 1;
+            aTextBox.Origin = origin;
+            aTextBox.Content = "TestContent";
+            aTextBox.Font = "Arial";
+            aTextBox.FontSize = 14;
+            TextBox anotherTextBox = new TextBox(1, 2,comments,origin,"TestContent","Arial",14);
+            bool result = aTextBox.Equals(anotherTextBox);
+            Assert.IsTrue(result);
+
+        }
+        //tiene un ancho, una altura, una lista de comentarios y un punto de origen
+        //Cuadro de texto: tiene un contenido, el cual puede tener distinto tamaño y tipo de letra.
+
+
     }
 }
