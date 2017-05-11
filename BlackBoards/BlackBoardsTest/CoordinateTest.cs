2b6325014a6a6fd8a393fdd75597093e20d28ae3
Federico Cetraro <facetraro@gmail.com>
2017-05-11 15:52:35 -0300

    Pruebas Coordinate Red.

---
+++ BlackBoards/BlackBoardsTest/CoordinateTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace BlackBoardsTest
+{
+    [TestClass]
+    public class CoordinateTest
+    {
+        [TestMethod]
+        public void testBuilderCoordinate()
+        {
+            int xAxis = 3;
+            int yAxis = 3;
+          
+            Coordinate aCoordinate = new Coordinate(xAxis, yAxis);
+            Coordinate otherCoordinate = new Coordinate();
+            otherCoordinate.XAxis = xAxis;
+            otherCoordinate.YAxis = yAxis;
+            
+            Assert.IsTrue(aCoordinate.Equals(otherCoordinate));
+        }
+    }
+    [TestMethod]
+    public void testEqualsCoordinate()
+    {
+        int xAxisVaule1 = 3;
+        int yAxisValue1 = 3;
+        int xAxisVaule2 = 5;
+        int yAxisValue2 = 3;
+
+        Coordinate aCoordinate = new Coordinate(xAxisValue1, yAxisValue1);
+        Coordinate otherCoordinate = new Coordinate(xAxisValue2, yAxisValue2);
+
+        Assert.IsFalse(aCoordinate.Equals(otherCoordinate));
+    }
+}
+}
