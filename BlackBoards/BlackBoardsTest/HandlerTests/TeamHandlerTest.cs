39000271a7008a4ebfaa647e36d8249685bce87a
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-12 02:34:49 -0300

    Pruebas TeamHandler red

---
+++ BlackBoards/BlackBoardsTest/HandlerTests/TeamHandlerTest.cs
@@ -0,0 +1,24 @@
+﻿using BlackBoards;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoardsTest.HandlerTests
+{
+    [TestClass]
+    public class TeamHandlerTest
+    {
+        [TestMethod]
+        public void TestTeamHandlerBuilder()
+        {
+            Team aTeam = new Team();
+            TeamHandler handler = new TeamHandler(aTeam);
+            bool result = aTeam.Equals(handler.Team);
+            Assert.IsTrue(result);
+
+        }
+    }
+}
