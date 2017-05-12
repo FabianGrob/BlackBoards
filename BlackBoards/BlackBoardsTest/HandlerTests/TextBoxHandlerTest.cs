8090cef35e66218057cf65b5e5fdd7f691945848
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-12 17:19:36 -0300

    Pruebas TextBoxHandler red

---
+++ BlackBoards/BlackBoardsTest/HandlerTests/TextBoxHandlerTest.cs
@@ -0,0 +1,24 @@
+﻿using BlackBoards;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoardsTest.HandlerTests
+{
+    [TestClass]
+    public class TextBoxHandlerTest
+    {
+        [TestMethod]
+        public void TestTextBoxHandler() {
+            TextBox aTbx = new TextBox();
+            TextBox otherTbx = new TextBox();
+            TextBoxHandler handler = new TextBoxHandler(tbx);
+            bool result = otherTbx.Equals(handler.TextBox);
+            Assert.IsTrue(result);
+
+        }
+    }
+}
