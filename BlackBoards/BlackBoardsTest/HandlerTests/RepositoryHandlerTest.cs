80e889e5718089e183d46d2d0d0ce0c6d7618da8
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-12 02:49:37 -0300

    Pruebas RepositoryHandler red

---
+++ BlackBoards/BlackBoardsTest/HandlerTests/RepositoryHandlerTest.cs
@@ -0,0 +1,27 @@
+﻿using BlackBoards;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoardsTest.HandlerTests
+{
+    [TestClass]
+    public class RepositoryHandlerTest
+    {
+        public int Administrator { get; private set; }
+
+        [TestMethod]
+        public void TestRepositoryHandler() {
+            List<Team> teams = new List<Team>();
+            List<User> users = new List<User>();
+            List<Admin> admins = new List<Admin>();
+            Repository repository = new Repository(admins,users,teams);
+            RepositoryHandler handler = new RepositoryHandler(repository);
+            bool result = repository.Equals(handler.Repository);
+
+        }
+    }
+}
