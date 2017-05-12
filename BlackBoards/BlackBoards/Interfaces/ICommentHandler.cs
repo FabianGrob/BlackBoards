e22a85e8540216cbadda3c7723c051ca319fdc82
Federico Cetraro <facetraro@gmail.com>
2017-05-12 16:27:19 -0300

    Creacion Interfaz CommentHandler.

---
+++ BlackBoards/BlackBoards/Interfaces/ICommentHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoards.Interfaces
+{
+    public interface ICommentHandler
+    {
+        void CreateComment(User anUser, string newComment);
+        void ResolveComment(User anUser);
+        void AddResolvingUser(User anUser);
+        void AddCommentingUser(User anUser);
+        void AddCommentingDate(DateTime aDate);
+        void AddResolvingDate(DateTime aDate);
+        void WriteComment(string commentToAdd);
+    }
+}
