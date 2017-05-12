eaef8a50b440aaa7ade022b2da715d8640c4a0c4
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-12 02:54:44 -0300

    Pruebas RepositoryHandler green

---
+++ BlackBoards/BlackBoards/Handlers/RepositoryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoards.Handlers
+{
+   public class RepositoryHandler
+    {
+        private Repository repository;
+
+        public RepositoryHandler(Repository theRepository) {
+            this.repository = theRepository;
+        }
+        public Repository Repository
+        {
+            get
+            {
+                return this.repository;
+            }
+            set
+            {
+                this.repository = value;
+            }
+        }
+    }
+}
