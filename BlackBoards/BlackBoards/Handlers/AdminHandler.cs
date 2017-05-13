8138cf2639f4fc51cfd3c805dd275e9b73d34365
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-13 20:59:18 -0300

    Pruebas AdminHandlerBuilder green

---
+++ BlackBoards/BlackBoards/Handlers/AdminHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoards.Handlers
+{
+    public class AdminHandler
+    {
+        private Admin admin;
+        public AdminHandler(Admin anAdmin) {
+            this.Admin = anAdmin;
+        }
+        public Admin Admin
+        {
+            get
+            {
+                return this.admin;
+            }
+            set
+            {
+                this.admin = value;
+            }
+        }
+
+    }
+}
