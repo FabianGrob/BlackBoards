28f69a2adb3dfc813d88da384775ff414c6a90e8
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-12 23:21:40 -0300

    Pruebas ItemHandler green

---
+++ BlackBoards/BlackBoards/Handlers/ItemHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoards.Handlers
+{
+    public class ItemHandler
+    {
+        private Item item;
+        public ItemHandler(Item anItem) {
+            this.item = anItem;
+        }
+        public Item Item
+        {
+            get
+            {
+                return this.item;
+            }
+            set
+            {
+                this.item = value;
+            }
+        }
+    }
+}
