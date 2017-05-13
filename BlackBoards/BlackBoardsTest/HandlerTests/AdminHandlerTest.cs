0b97cdec4995d61e770f5db774b324a88970254b
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-13 20:54:02 -0300

    Pruebas AdminHandlerBuilder red

---
+++ BlackBoards/BlackBoardsTest/HandlerTests/AdminHandlerTest.cs
@@ -0,0 +1,24 @@
+﻿using BlackBoards;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoardsTest.HandlerTests
+{
+    [TestClass]
+    public class AdminHandlerTest
+    {
+        [TestMethod]
+        public void TestAdminHandlerBuilder() {
+
+            Admin admin = new Admin();
+            AdminHandler handler = new AdminHandler(admin);
+
+            bool result = admin.Equals(handler.Admin);
+            Assert.IsTrue(result);
+        }
+    }
+}
