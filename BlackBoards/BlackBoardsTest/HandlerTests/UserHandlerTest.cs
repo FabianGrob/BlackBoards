8c752eba342f6e0a01d55c8347d7ba9b651e7ca0
Federico Cetraro <facetraro@gmail.com>
2017-05-13 21:55:37 -0300

    Prueba RemoveItemBlackBoard red

--- BlackBoards/BlackBoardsTest/HandlerTests/UserHandlerTest.cs
+++ BlackBoards/BlackBoardsTest/HandlerTests/UserHandlerTest.cs
@@ -90,6 +90,49 @@ namespace BlackBoardsTest
             //assertion
             Assert.IsFalse(result);
         }
+        [TestMethod]
+        public void TestRemoveItemBlackBoard()
+        {
+            User u = new Collaborator();
+            UserHandler userHandler = new UserHandler(u);
+            Item item = new TextBox();
+            BlackBoard blackBoard = new BlackBoard();
+            userHandler.AddItemToBlackBoard(blackBoard, item);
+            userHandler.RemoveItemBlackBoard(blackBoard, item);
+            bool result = blackBoard.ItemList.Count == 0;
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestRemoveItemBlackBoardBool()
+        {
+            User u = new Collaborator();
+            UserHandler userHandler = new UserHandler(u);
+            Item item = new TextBox();
+            BlackBoard blackBoard = new BlackBoard();
+            userHandler.AddItemToBlackBoard(blackBoard, item);  
+            bool result = userHandler.RemoveItemBlackBoard(blackBoard, item);
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestRemoveInvalidItemBlackBoard()
+        {
+
+            User u = new Collaborator();
+            UserHandler userHandler = new UserHandler(u);
+            Item item = new TextBox();
+            Item anotherItem = new TextBox();
+            Dimension newDimension = new Dimension(2, 2);
+            item.Dimension = newDimension;
+            BlackBoard blackBoard = new BlackBoard();
+            userHandler.AddItemToBlackBoard(blackBoard, item);
+            userHandler.RemoveItemBlackBoard(blackBoard, anotherItem);
+            bool result = blackBoard.ItemList.Count == 1;
+
+            Assert.IsTrue(result);
+        }
+        
 
     }
     
