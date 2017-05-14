f18abc8bf0ac395bc1973d5171c5d5db6cdc327e
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-05-13 22:42:25 -0300

    Pruebas DeleteUser en AdminHandler red

--- BlackBoards/BlackBoardsTest/HandlerTests/AdminHandlerTest.cs
+++ BlackBoards/BlackBoardsTest/HandlerTests/AdminHandlerTest.cs
@@ -13,7 +13,8 @@ namespace BlackBoardsTest.HandlerTests
     public class AdminHandlerTest
     {
         [TestMethod]
-        public void TestAdminHandlerBuilder() {
+        public void TestAdminHandlerBuilder()
+        {
             //instance
             Admin admin = new Admin();
             AdminHandler handler = new AdminHandler(admin);
@@ -22,18 +23,19 @@ namespace BlackBoardsTest.HandlerTests
             Assert.IsTrue(result);
         }
         [TestMethod]
-        public void TestCreateCollaboratorCorrectly() {
+        public void TestCreateCollaboratorCorrectly()
+        {
             //instance
             Repository repository = new Repository();
             Admin anAdmin = new Admin();
             AdminHandler handler = new AdminHandler(anAdmin);
-            string name="aNewName";
-            string lastName="aLastName";
-            string email="AnEmail";
-            DateTime birthDate=DateTime.Today;
-            string password="aPassword";
+            string name = "aNewName";
+            string lastName = "aLastName";
+            string email = "AnEmail";
+            DateTime birthDate = DateTime.Today;
+            string password = "aPassword";
             //assertion
-            bool result =handler.CreateCollaborator(name,lastName,email,birthDate,password, repository);
+            bool result = handler.CreateCollaborator(name, lastName, email, birthDate, password, repository);
             Assert.IsTrue(result);
         }
         [TestMethod]
@@ -54,7 +56,8 @@ namespace BlackBoardsTest.HandlerTests
             Assert.IsFalse(result);
         }
         [TestMethod]
-        public void TestModifyUserCorrectly() {
+        public void TestModifyUserCorrectly()
+        {
             //instance
             Repository repository = new Repository();
             Admin anAdmin = new Admin();
@@ -67,7 +70,7 @@ namespace BlackBoardsTest.HandlerTests
             handler.CreateCollaborator(name, lastName, email, birthDate, password, repository);
             string modEmail = "AModifiedEmail";
             //assertion
-            bool result = handler.ModifyUser(email,name, lastName, modEmail, birthDate, password, repository);
+            bool result = handler.ModifyUser(email, name, lastName, modEmail, birthDate, password, repository);
             Assert.IsTrue(result);
 
         }
@@ -86,7 +89,7 @@ namespace BlackBoardsTest.HandlerTests
             string password = "aPassword";
             handler.CreateCollaborator(name, lastName, email, birthDate, password, repository);
             string modEmail = "AModifiedEmail";
-            handler.ModifyUser(email,name, lastName, modEmail, birthDate, password, repository);
+            handler.ModifyUser(email, name, lastName, modEmail, birthDate, password, repository);
             //assertion
             bool result = repHandler.getSepcificUser(modEmail).Name.Equals(name);
             Assert.IsTrue(result);
@@ -106,10 +109,61 @@ namespace BlackBoardsTest.HandlerTests
             string password = "aPassword";
             string modEmail = "AModifiedEmail";
             //assertion
-            bool result = handler.ModifyUser(email,name, lastName, modEmail, birthDate, password, repository);
+            bool result = handler.ModifyUser(email, name, lastName, modEmail, birthDate, password, repository);
             Assert.IsFalse(result);
 
         }
+        [TestMethod]
+        public void TestDeleteUserCorrectly()
+        {
+            //instance
+            Repository repository = new Repository();
+            Admin anAdmin = new Admin();
+            AdminHandler handler = new AdminHandler(anAdmin);
+            string name = "aNewName";
+            string lastName = "aLastName";
+            string email = "AnEmail";
+            DateTime birthDate = DateTime.Today;
+            string password = "aPassword";
+            handler.CreateCollaborator(name, lastName, email, birthDate, password, repository);
+            bool result =handler.DeleteUser(email,repository);
+            Assert.IsTrue(result);
 
+        }
+        [TestMethod]
+        public void TestDeleteUserCheck()
+        {
+            //instance
+            Repository repository = new Repository();
+            Admin anAdmin = new Admin();
+            AdminHandler handler = new AdminHandler(anAdmin);
+            string name = "aNewName";
+            string lastName = "aLastName";
+            string email = "AnEmail";
+            DateTime birthDate = DateTime.Today;
+            string password = "aPassword";
+            handler.CreateCollaborator(name, lastName, email, birthDate, password, repository);
+            handler.DeleteUser(email, repository);
+            bool result = repository.UserList.Count == 0;
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+        public void TestDeleteUserDoesntExists()
+        {
+            //instance
+            Repository repository = new Repository();
+            Admin anAdmin = new Admin();
+            AdminHandler handler = new AdminHandler(anAdmin);
+            string name = "aNewName";
+            string lastName = "aLastName";
+            string email = "AnEmail";
+            DateTime birthDate = DateTime.Today;
+            string password = "aPassword";
+            handler.DeleteUser(email, repository);
+            bool result = repository.UserList.Count == 0;
+            Assert.IsFalse(result);
+
+        }
     }
 }
