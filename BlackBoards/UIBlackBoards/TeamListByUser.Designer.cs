80ddd9b70629a5f212fd1f70b993b159964652f0
Federico Cetraro <facetraro@gmail.com>
2017-05-15 04:43:43 -0300

    Se crea ventana TeamListByUser

---
+++ BlackBoards/UIBlackBoards/TeamListByUser.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace UIBlackBoards
+{
+    partial class TeamListByUser
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.listBoxTeams = new System.Windows.Forms.ListBox();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(14, 21);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(48, 13);
+            this.labelTitle.TabIndex = 8;
+            this.labelTitle.Text = "Equipos:";
+            // 
+            // listBoxTeams
+            // 
+            this.listBoxTeams.FormattingEnabled = true;
+            this.listBoxTeams.Location = new System.Drawing.Point(14, 42);
+            this.listBoxTeams.Name = "listBoxTeams";
+            this.listBoxTeams.Size = new System.Drawing.Size(236, 225);
+            this.listBoxTeams.TabIndex = 7;
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(175, 281);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelect.TabIndex = 5;
+            this.buttonSelect.Text = "Seleccionar";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // TeamListByUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.listBoxTeams);
+            this.Controls.Add(this.buttonSelect);
+            this.Name = "TeamListByUser";
+            this.Size = new System.Drawing.Size(262, 317);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.ListBox listBoxTeams;
+        private System.Windows.Forms.Button buttonSelect;
+    }
+}
---
+++ BlackBoards/UIBlackBoards/TeamListByUser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BlackBoards;
+using BlackBoards.Handlers;
+
+namespace UIBlackBoards
+{
+    public partial class TeamListByUser : UserControl
+    {
+        private User logged;
+        private Repository theRepository;
+        private Panel panelContainer;
+        public TeamListByUser(User anUser, Repository aRepository, Panel container)
+        {
+            InitializeComponent();
+            logged = anUser;
+            theRepository = aRepository;
+            panelContainer = container;
+            RepositoryHandler rHandler = new RepositoryHandler(aRepository);
+            List<Team> listOfTeamsByUser = rHandler.getUserTeams(logged);
+            foreach (Team actualteam in listOfTeamsByUser)
+            {
+                listBoxTeams.Items.Add(actualteam);
+            }
+        }
+
+        private bool hasSelectedATeam()
+        {
+            int selectedIndex = listBoxTeams.SelectedIndex;
+            bool hasSelected = (selectedIndex != -1);
+            return hasSelected;
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            if (hasSelectedATeam())
+            {
+                Team selectedTeam = (Team) listBoxTeams.SelectedItem;
+                panelContainer.Controls.Clear();
+              
+            }
+            else
+            {
+                MessageBox.Show("No se selecciono ningun equipo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
