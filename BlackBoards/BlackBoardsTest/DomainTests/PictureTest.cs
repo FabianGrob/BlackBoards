cd0879568a96aa6928442fdd3f42051e5772f2c5
Federico Cetraro <facetraro@gmail.com>
2017-06-10 15:08:45 -0300

    Prueba TestPictureValidationReturnFalse red

--- BlackBoards/BlackBoardsTest/DomainTests/PictureTest.cs
+++ BlackBoards/BlackBoardsTest/DomainTests/PictureTest.cs
@@ -1,5 +1,6 @@
 ﻿using BlackBoards;
 using BlackBoards.Domain;
+using BlackBoards.Domain.BlackBoards;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Drawing;
@@ -10,13 +11,23 @@ namespace BlackBoardsTest
     [TestClass]
     public class PictureTest
     {
-        [TestMethod]
-        public void TestPictureBuilder() {
-            Picture aPic = new Picture();
-            List<Comment> comments =new List<Comment>();
-            aPic.Dimension = new Dimension(1,1);
+        private Picture setUpPicture()
+        {
+            Picture aSetUpPicture = new Picture();
+            List<Comment> comments = new List<Comment>();
+            aSetUpPicture.Dimension = new Dimension(50, 50);
             Coordinate origin = new Coordinate();
-
+            aSetUpPicture.Origin = origin;
+            aSetUpPicture.Description = "Example Description";
+            return aSetUpPicture;
+        }
+        [TestMethod]
+        public void TestPictureBuilder()
+        {
+            Picture aPic = new Picture();
+            List<Comment> comments = new List<Comment>();
+            aPic.Dimension = new Dimension(1, 1);
+            Coordinate origin = new Coordinate();
             aPic.Origin = origin;
             aPic.Comments = comments;
             string proyectPath = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
@@ -38,10 +49,11 @@ namespace BlackBoardsTest
             Assert.IsTrue(aPic.IsPicture());
         }
         [TestMethod]
-        public void TestPictureNotEquals() {
+        public void TestPictureNotEquals()
+        {
             Picture aPic = new Picture();
 
-            aPic.Dimension = new Dimension(1,1);
+            aPic.Dimension = new Dimension(1, 1);
             aPic.Comments = new List<Comment>();
             Coordinate origin = new Coordinate();
             aPic.Origin = origin;
@@ -51,6 +63,15 @@ namespace BlackBoardsTest
             Assert.IsFalse(result);
 
         }
+        [TestMethod]
+        public void TestPictureValidationReturnFalse()
+        {
+            Picture aPic = setUpPicture();
+            aPic.Img = null;
+            ValidationReturn validationResult = aPic.IsValid();
+            bool result = validationResult.Validation;
+            Assert.IsFalse(result);
+        }
     }
-    
+
 }
