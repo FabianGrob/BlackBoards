ba693041a6453b35fb5d635148b84e16bed4fadb
Federico Cetraro <facetraro@gmail.com>
2017-06-12 18:17:42 -0300

    TestBuilderScoreUserInTeamTest green

---
+++ BlackBoards/BlackBoards/Domain/ScoreUserInTeam.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoards.Domain
+{
+    public class ScoreUserInTeam
+    {
+        private Team team;
+        private User user;
+        private Score score;
+        public ScoreUserInTeam()
+        {
+            this.team = new Team();
+            this.user = new Admin();
+            this.score = new Score();
+        }
+        public Team Team
+        {
+            get
+            {
+                return this.team;
+            }
+            set
+            {
+                this.team = value;
+            }
+        }
+        public User User
+        {
+            get
+            {
+                return this.user;
+            }
+            set
+            {
+                this.user = value;
+            }
+        }
+        public Score Score
+        {
+            get
+            {
+                return this.score;
+            }
+            set
+            {
+                this.score = value;
+            }
+        }
+        public override bool Equals(object aScoreUserInTeam)
+        {
+            if (aScoreUserInTeam == null)
+            {
+                return false;
+            }
+            ScoreUserInTeam otherScoreUserInTeam = aScoreUserInTeam as ScoreUserInTeam;
+            if ((System.Object)otherScoreUserInTeam == null)
+            {
+                return false;
+            }
+            bool scoreEquals = (this.score.Equals(otherScoreUserInTeam.Score));
+            bool teamEquals = (this.team.Equals(otherScoreUserInTeam.Team));
+            bool userEquals = (this.user.Equals(otherScoreUserInTeam.User));
+            return (scoreEquals && teamEquals && userEquals);
+        }
+    }
+}
--- BlackBoards/BlackBoardsTest/DomainTests/ScoreUserInTeamTest.cs
+++ BlackBoards/BlackBoardsTest/DomainTests/ScoreUserInTeamTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BlackBoards;
+using BlackBoards.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +13,33 @@ namespace BlackBoardsTest
     [TestClass]
     public class ScoreUserInTeamTest
     {
-        private ScoreUserInTeamTest setUp()
+        private ScoreUserInTeam setUp()
         {
-            ScoreUserInTeamTest scoreUserInTeamTest = new ScoreUserInTeamTest();
+            ScoreUserInTeam scoreUserInTeamTest = new ScoreUserInTeam();
+            Score newScore = new Score(5,4,3,1,7);
+            String name = "testName";
+            DateTime creationDate = new DateTime();
+            String description = "testDescription";
+            int maximumUsers = 10;
+            List<User> members = new List<User>();
+            List<BlackBoard> boards = new List<BlackBoard>();
+            Team newTeam = new Team(name, creationDate, description, maximumUsers, members, boards);
+            String lastName = "testLastName";
+            String email = "testEmail";
+            DateTime birthDate = new DateTime();
+            String password = "testPassword";
+            User newUser = new Admin(name, lastName, email, birthDate, password);
+            scoreUserInTeamTest.Team = newTeam;
+            scoreUserInTeamTest.Score = newScore;
+            scoreUserInTeamTest.User = newUser;
             return scoreUserInTeamTest;
         }
         [TestMethod]
         public void TestBuilderScoreUserInTeamTest()
         {
             //instance
-            ScoreUserInTeamTest aScoreUserInTeamTest = setUp();
-            ScoreUserInTeamTest anotherScoreUserInTeamTest = setUp();
+            ScoreUserInTeam aScoreUserInTeamTest = setUp();
+            ScoreUserInTeam anotherScoreUserInTeamTest = setUp();
             bool result = aScoreUserInTeamTest.Equals(anotherScoreUserInTeamTest);
             //assertion
             Assert.IsTrue(result);
