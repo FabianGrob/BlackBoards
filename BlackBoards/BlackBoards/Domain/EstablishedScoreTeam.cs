3877bcec834ed67213dcbab6e2571d510e7f9724
Federico Cetraro <facetraro@gmail.com>
2017-06-12 18:50:07 -0300

    TestBuilderEstablishedScoreTeam green

---
+++ BlackBoards/BlackBoards/Domain/EstablishedScoreTeam.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoards.Domain
+{
+    public class EstablishedScoreTeam
+    {
+        private Team team;
+        private Score score;
+        public EstablishedScoreTeam()
+        {
+            this.team = new Team();
+            this.score = new Score();
+        }
+        public Team Team
+        {
+            get
+            {
+                return this.team;
+            }
+            set
+            {
+                this.team = value;
+            }
+        }
+        public Score Score
+        {
+            get
+            {
+                return this.score;
+            }
+            set
+            {
+                this.score = value;
+            }
+        }
+        public override bool Equals(object aEstablishedScoreTeam)
+        {
+            if (aEstablishedScoreTeam == null)
+            {
+                return false;
+            }
+            EstablishedScoreTeam anotherEstablishedScoreTeam = aEstablishedScoreTeam as EstablishedScoreTeam;
+            if ((System.Object)anotherEstablishedScoreTeam == null)
+            {
+                return false;
+            }
+            return this.team.Equals(anotherEstablishedScoreTeam.Team);
+        }
+    }
+}
