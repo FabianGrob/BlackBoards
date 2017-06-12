d49d34f830c6974784e0d00cccc89622ba5609d3
Federico Cetraro <facetraro@gmail.com>
2017-06-12 16:49:19 -0300

    TestBuilderScore red

---
+++ BlackBoards/BlackBoardsTest/DomainTests/ScoreTest.cs
@@ -0,0 +1,23 @@
+﻿using BlackBoards.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoardsTest
+{
+    [TestClass]
+    public class ScoreTest
+    {
+        [TestMethod]
+        public void TestBuilderScore()
+        {
+            Score scoreTest = new Score();
+            Score anotherScoreTest = new Score(0,0,0,0,0);
+            bool result = scoreTest.Equals(anotherScoreTest);
+            Assert.IsTrue(result);
+        }
+    }
+}
