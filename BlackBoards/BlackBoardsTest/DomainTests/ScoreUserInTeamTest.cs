301890613469664ca06cf1305ca626e4a7a20124
Federico Cetraro <facetraro@gmail.com>
2017-06-12 18:02:01 -0300

    TestBuilderScoreUserInTeamTest red

---
+++ BlackBoards/BlackBoardsTest/DomainTests/ScoreUserInTeamTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BlackBoardsTest
+{
+    [TestClass]
+    public class ScoreUserInTeamTest
+    {
+        private ScoreUserInTeamTest setUp()
+        {
+            ScoreUserInTeamTest scoreUserInTeamTest = new ScoreUserInTeamTest();
+            return scoreUserInTeamTest;
+        }
+        [TestMethod]
+        public void TestBuilderScoreUserInTeamTest()
+        {
+            //instance
+            ScoreUserInTeamTest aScoreUserInTeamTest = setUp();
+            ScoreUserInTeamTest anotherScoreUserInTeamTest = setUp();
+            bool result = aScoreUserInTeamTest.Equals(anotherScoreUserInTeamTest);
+            //assertion
+            Assert.IsTrue(result);
+        }
+    }
+}
