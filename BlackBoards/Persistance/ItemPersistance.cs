8771358d9bcad5076a5bc146cf4fd1b7e031fad1
FABIANG-PC\Fabian <fabigro10@gmail..com>
2017-06-20 03:09:55 -0300

    Agregamos clase ItemPersistance

---
+++ BlackBoards/Persistance/ItemPersistance.cs
@@ -0,0 +1,123 @@
+﻿using BlackBoards;
+using Persistance.PersistanceException;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    public class ItemPersistance
+    {
+        public void AddItem(Item anItem)
+        {
+            try
+            {
+                using (BlackBoardsContext dbContext = new BlackBoardsContext())
+                {
+                    dbContext.items.Add(anItem);
+                    dbContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new PersistanceItemException("Error en la base de datos. Imposible agregar el Elemento");
+            }
+
+        }
+        public void Delete(Item anItem)
+        {
+            try
+            {
+                using (BlackBoardsContext dbContext = new BlackBoardsContext())
+                {
+
+                    dbContext.items.Attach(anItem);
+                    dbContext.Entry(anItem).State = EntityState.Deleted;
+                    dbContext.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new PersistanceItemException("Error de base de datos: No se pudo eliminar el Elemento.");
+            }
+
+        }
+        public void ModifyItem(Item anItem)
+        {
+            try
+            {
+                using (BlackBoardsContext dbContext = new BlackBoardsContext())
+                {
+
+                    if (this.Exists(anItem))
+                    {
+                        Item anotherItem = this.GetItem(anItem.IDItem);
+                        anotherItem.Dimension = anItem.Dimension;
+                        anotherItem.Origin = anItem.Origin;
+                        dbContext.items.Attach(anotherItem);
+                        dbContext.Entry(anotherItem).State = EntityState.Modified;
+                        dbContext.SaveChanges();
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                throw new PersistanceUserException("Error en la base de datos. Imposible Modificar el Elemento ");
+            }
+        }
+        private Item GetItem(int id)
+        {
+            try
+            {
+                using (BlackBoardsContext dbContext = new BlackBoardsContext())
+                {
+                    return dbContext.items.Find(id);
+                }
+            }
+            catch (Exception)
+            {
+                throw new PersistanceUserException("Error de base de datos: No se pudo obtener el Elemento.");
+
+            }
+        }
+        public void Empty()
+        {
+            try
+            {
+                using (BlackBoardsContext dbContext = new BlackBoardsContext())
+                {
+                    List<Item> items = dbContext.items.ToList();
+                    foreach (Item actualItem in items)
+                    {
+                        Item toDelete = dbContext.items.Find(actualItem.IDItem);
+                        dbContext.items.Remove(toDelete);
+                    }
+                    dbContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw new PersistanceUserException("Error en la base de datos. Imposible vaciar valores de elementos");
+            }
+        }
+        public bool Exists(Item anItem)
+        {
+            try
+            {
+                using (BlackBoardsContext dbContext = new BlackBoardsContext())
+                {
+                    List<Item> items = dbContext.items.ToList<Item>();
+                    return items.Contains(anItem);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new PersistanceUserException("Error de base de datos: No se pudo determinar si el elemento existe.");
+            }
+        }
+    }
+}
