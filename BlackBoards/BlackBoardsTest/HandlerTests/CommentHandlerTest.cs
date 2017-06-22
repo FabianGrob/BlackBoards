41259dc0321ed23e9d13416201087aa73506c103
Federico Cetraro <facetraro@gmail.com>
2017-06-22 16:35:47 -0300

    Se amplia cobertura de pruebas WasResolved

--- BlackBoards/BlackBoardsTest/HandlerTests/CommentHandlerTest.cs
+++ BlackBoards/BlackBoardsTest/HandlerTests/CommentHandlerTest.cs
@@ -14,62 +14,23 @@ namespace BlackBoardsTest.HandlersTests
     [TestClass]
     public class CommentHandlerTest
     {
-       /* [TestMethod]
-        public void TestCreateComment()
+        [TestMethod]
+        public void TestResolvedComment()
         {
-            User anUser = new Admin();
-            string comment = "This is a test comment.";
-            CommentHandler commentHandler= new CommentHandler();
-            commentHandler.CreateComment(anUser, comment);
-            bool userEquals = anUser.Equals(commentHandler.Comment.CommentingUser);
-            bool commentEquals = comment.Equals(commentHandler.Comment.WrittenComment);
-            bool dateEquals = DateTime.Today.Equals(commentHandler.Comment.CommentingDate);
-
-            Assert.IsTrue(userEquals && commentEquals && dateEquals);         
+            Comment aCommentTest = new Comment();
+            aCommentTest.CommentingDate = DateTime.Now;
+            aCommentTest.ResolvingDate = DateTime.Now;
+            CommentHandler commentHandler = new CommentHandler(aCommentTest);
+            Assert.IsTrue(commentHandler.WasResolved());
         }
         [TestMethod]
-        public void TestResolveComment()
+        public void TestUnResolvedComment()
         {
-            User anUser = new Admin();
-            CommentHandler commentHandler = new CommentHandler();
-            commentHandler.ResolveComment(anUser);
-            bool userEquals = anUser.Equals(commentHandler.Comment.ResolvingUser);
-            bool dateEquals = DateTime.Today.Equals(commentHandler.Comment.ResolvingDate);
-            Assert.IsTrue(userEquals && dateEquals);
+            Comment aCommentTest = new Comment();
+            aCommentTest.CommentingDate = DateTime.Now;
+            aCommentTest.ResolvingDate = DateTime.Now.AddDays(-1);
+            CommentHandler commentHandler = new CommentHandler(aCommentTest);
+            Assert.IsFalse(commentHandler.WasResolved());
         }
-        [TestMethod]
-        public void TestResolveCommentBool()
-        {
-            User anUser = new Admin();
-            CommentHandler commentHandler = new CommentHandler();
-            bool result = commentHandler.ResolveComment(anUser);
-            Assert.IsTrue(result);
-        }
-        [TestMethod]
-        public void TestResolveResolvedComment()
-        {
-            User anUser = new Admin();
-            CommentHandler commentHandler = new CommentHandler();
-            commentHandler.ResolveComment(anUser);
-            User anotherUser = new Admin();
-            anotherUser.Email = "anotherUser@test.com";
-            commentHandler.ResolveComment(anUser);
-            commentHandler.ResolveComment(anotherUser);
-            bool userEquals = anUser.Equals(commentHandler.Comment.ResolvingUser);
-            bool dateEquals = DateTime.Today.Equals(commentHandler.Comment.ResolvingDate);
-            Assert.IsTrue(userEquals && dateEquals);
-        }
-        [TestMethod]
-        public void TestResolveResolvedCommentBool()
-        {
-            User anUser = new Admin();
-            CommentHandler commentHandler = new CommentHandler();
-            commentHandler.ResolveComment(anUser);
-            User anotherUser = new Admin();
-            anotherUser.Email = "anotherUser@test.com";
-            commentHandler.ResolveComment(anUser);
-            bool result = commentHandler.ResolveComment(anotherUser); 
-            Assert.IsFalse(result);
-        }*/
     }
 }
