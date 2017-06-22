a5aa77ab180ae2b012b7a6efa059f68fce1a58c7
Federico Cetraro <facetraro@gmail.com>
2017-06-22 18:16:06 -0300

    Pruebas ScoreCalculatorTest red

---
+++ BlackBoards/Logic/ScoresCalculator.cs
@@ -0,0 +1,33 @@
+﻿using BlackBoards.Domain;
+using BlackBoards.Domain.BlackBoards;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBoards.Handlers
+{
+    public class ScoresCalculator
+    {
+        private EstablishedScoreTeam establishedScore;
+        private List<ScoreUserInTeam> scoreUser;
+        public ScoresCalculator(EstablishedScoreTeam establishedScore, List<ScoreUserInTeam> scoreUser)
+        {
+            this.establishedScore = establishedScore;
+            this.scoreUser = scoreUser;
+        }
+        public int CalculateScoreOfATeam()
+        {
+            int totalScoreTeam = 0;
+            foreach (ScoreUserInTeam actualScoreTeam in this.scoreUser)
+            {
+                Score actualScore = new Domain.Score(actualScoreTeam.Score.CreateBlackBoard, actualScoreTeam.Score.DeleteBlackBoard,
+                    actualScoreTeam.Score.AddItem, actualScoreTeam.Score.AddComment, actualScoreTeam.Score.SolveComment);
+                totalScoreTeam = totalScoreTeam + actualScore.CalculateScore(this.establishedScore.score);
+            }
+            return totalScoreTeam;
+        }
+    }
+}
