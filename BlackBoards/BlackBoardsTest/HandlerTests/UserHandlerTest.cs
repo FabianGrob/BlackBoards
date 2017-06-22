92f66aa308d6714b95bc1dac734a58b0e4532c46
Federico Cetraro <facetraro@gmail.com>
2017-06-22 13:58:35 -0300

    Ampliamos cobertura de las pruebas de TestMoveItem

--- BlackBoards/BlackBoardsTest/HandlerTests/UserHandlerTest.cs
+++ BlackBoards/BlackBoardsTest/HandlerTests/UserHandlerTest.cs
@@ -207,5 +207,28 @@ namespace BlackBoardsTest
             //assertion
             Assert.IsTrue(aCoordinate.Equals(theItem.Origin));
         }
+        [TestMethod]
+        public void TestMoveItemInvalid()
+        {
+            //instance
+            Initialize();
+            AdminPersistance adminContext = new AdminPersistance();
+            User generatedUser = adminContext.GetUserByEmail("generatedEmail@email.com");
+            UserHandler handler = new UserHandler(generatedUser);
+            BlackBoardPersistance blackBoardContext = new BlackBoardPersistance();
+            BlackBoard generatedBlackBoard = blackBoardContext.GetBlackBoardByName("generatedBoard");
+            TextBox textBox = new TextBox();
+            textBox.blackBoardBelongs = generatedBlackBoard;
+            textBox.Font = "Arial";
+            textBox.FontSize = 12;
+            textBox.Content = "ThisIsATest";
+            handler.AddItemToBlackBoard(generatedBlackBoard, textBox);
+            TextBox theItem = generatedBlackBoard.itemList.ElementAt(0) as TextBox;
+            Coordinate aCoordinate = new Coordinate(9999, 50);
+            handler.MoveItemBlackBoard(generatedBlackBoard, theItem, aCoordinate);
+            CleanDB(new UserPersistance());
+            //assertion
+            Assert.IsFalse(aCoordinate.Equals(theItem.Origin));
+        }
     }
 }
